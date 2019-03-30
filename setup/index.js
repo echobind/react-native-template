33b8d866a709b546e2f8e7dd41432630af21f166
Isaiah Grey <isaiah@echobind.com>
2019-03-30 09:16:50 -0700

    feat(auto-version): automate initial version for iOS + Android projects to allow fastlane versioning (#47)
    * fix(.babelrc removal): newer RN projects init with babel.config.js instead of .babelrc
    
    * fix(.babelrc removal): newer RN projects init with babel.config.js instead of .babelrc
    
    * feat(auto-version): automate initial version for iOS + Android projects to allow fastlane versioning
    
    * refactor(react-native-version): use npx to set initial version via react-native-version

--- setup/index.js
+++ setup/index.js
@@ -31,12 +31,16 @@ execSync('rm -rf ios/HelloWorld/Base.lproj/LaunchScreen.xib', { cwd: rootDirecto
 execSync('yarn hygen setup splashscreen');
 
 console.log('\n🗑  Removing cruft...');
-deleteFile('../.babelrc'); // metro bundler version uses babel.config.js
 deleteFile('../.flowconfig');
 deleteFile('../App.js');
 deleteFile('../.gitattributes'); // not sure why this is here?
 execSync('rm -rf setup', { cwd: rootDirectory });
 execSync('rm -rf .git', { cwd: rootDirectory }); // blow away old repo if there
+
+console.log('\n📱 Setting initial version @0.0.1 ...');
+execSync('npx react-native-version --never-increment-build', {
+  cwd: rootDirectory,
+});
 
 console.log('\n📝 Committing project...');
 execSync(
