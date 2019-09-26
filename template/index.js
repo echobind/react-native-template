fa85d8fe8c9555f7ecf92202eb53e84451199e07
Joe Previte <joe@echobind.com>
2019-09-26 15:48:53 -0700

    refactor: remove postInitScript and move

--- template/index.js
+++ template/index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
-import { name as appName } from './app.json';
+import { name as appName } from './app.json.js';
 
 AppRegistry.registerComponent(appName, () => App);
