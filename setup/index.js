323e6bf2785dda1817710d94e793ea99befda58f
Joe Previte <joe@echobind.com>
2019-09-26 10:37:13 -0700

    refactor: move files to template

--- setup/index.js
+++ setup/index.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
-const packageJson = require('../package.json');
-const { prompt } = require('enquirer');
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+const { prompt } = require("enquirer");
 
 async function setup() {
   const deleteFile = fileName => fs.unlinkSync(path.join(__dirname, fileName));
-  const deleteDirectory = dirName => fs.rmdirSync(path.join(__dirname, dirName));
-  const writeFile = (fileName, data) => fs.writeFileSync(path.join(__dirname, fileName), data);
+  const deleteDirectory = dirName =>
+    fs.rmdirSync(path.join(__dirname, dirName));
+  const writeFile = (fileName, data) =>
+    fs.writeFileSync(path.join(__dirname, fileName), data);
 
   console.log('\n🤔 Checking system setup and prerequisites...');
   try {
@@ -20,21 +21,21 @@ async function setup() {
   console.log('\n📝  Configuring project display name and bundle identifier...');
   const { displayName, bundleIdentifer } = await prompt([
     {
-      type: 'input',
-      name: 'displayName',
-      message: 'App Display Name (ProjectName):',
+      type: "input",
+      name: "displayName",
+      message: "App Display Name (ProjectName):"
     },
     {
-      type: 'input',
-      name: 'bundleIdentifer',
-      message: `App Bundle Identifier (com.${displayName.toLowerCase()}.mobile):`,
-    },
+      type: "input",
+      name: "bundleIdentifer",
+      message: `App Bundle Identifier (com.${displayName.toLowerCase()}.mobile):`
+    }
   ]);
 
   const { confirmed } = await prompt({
-    type: 'confirm',
-    name: 'confirmed',
-    message: `Continue with => App Display Name: ${displayName} | App Bundle Identifer: ${bundleIdentifer}`,
+    type: "confirm",
+    name: "confirmed",
+    message: `Continue with => App Display Name: ${displayName} | App Bundle Identifer: ${bundleIdentifer}`
   });
 
   if (!confirmed) {
@@ -44,30 +45,22 @@ async function setup() {
   }
 
   const { shouldInitializeGitRepository } = await prompt({
-    type: 'confirm',
-    name: 'shouldInitializeGitRepository',
-    message: 'Would you like to initialize this project as a git repository?',
+    type: "confirm",
+    name: "shouldInitializeGitRepository",
+    message: "Would you like to initialize this project as a git repository?"
   });
 
-  console.log('🔄  Setting up...');
-  // add our update to package.json
-  const scripts = require('./scripts.json');
-  packageJson.scripts = { ...packageJson.scripts, ...scripts };
-  packageJson.jest = require('./jest.json');
-  packageJson['lint-staged'] = require('./lintStaged.json');
+  console.log("🔄  Setting up...");
 
-  console.log('\n📝  Writing package.json...');
-  writeFile('../package.json', JSON.stringify(packageJson, null, 2));
+  console.log("\n🛠  Setting up fastlane and installing app icons...");
+  const rootDirectory = path.join(__dirname, "../");
+  execSync("bundle install", { cwd: rootDirectory });
+  execSync("bundle exec fastlane ios icon", { cwd: rootDirectory });
+  execSync("bundle exec fastlane android icon", { cwd: rootDirectory });
 
-  console.log('\n🛠  Setting up fastlane and installing app icons...');
-  const rootDirectory = path.join(__dirname, '../');
-  execSync('bundle install', { cwd: rootDirectory });
-  execSync('bundle exec fastlane ios icon', { cwd: rootDirectory });
-  execSync('bundle exec fastlane android icon', { cwd: rootDirectory });
-
-  console.log('\n Updating project display name and bundle identifier...');
+  console.log("\n Updating project display name and bundle identifier...");
   execSync(`npx react-native-rename ${displayName} -b ${bundleIdentifer}`, {
-    cwd: rootDirectory,
+    cwd: rootDirectory
   });
 
   execSync(
@@ -75,54 +68,58 @@ async function setup() {
     { cwd: rootDirectory },
   );
 
-  console.log('\n☕  Installing Cocoapods...');
-  execSync('bundle exec pod install', { cwd: 'ios' });
+  console.log("\n☕  Installing Cocoapods...");
+  execSync("bundle exec pod install", { cwd: "ios" });
 
-  console.log('\n🍎🌊  Setting up ios splash screens...');
-  execSync('rm -rf ios/FamilyDirectedTest/Base.lproj/LaunchScreen.xib', { cwd: rootDirectory });
+  console.log("\n🍎🌊  Setting up ios splash screens...");
+  execSync("rm -rf ios/FamilyDirectedTest/Base.lproj/LaunchScreen.xib", {
+    cwd: rootDirectory
+  });
   execSync(
-    `HYGEN_OVERWRITE=1 yarn hygen setup splashscreen ios --displayName ${displayName} --bundleIdentifer ${bundleIdentifer}`,
+    `HYGEN_OVERWRITE=1 yarn hygen setup splashscreen ios --displayName ${displayName} --bundleIdentifer ${bundleIdentifer}`
   );
 
-  console.log('\n🤖🌊  Setting up android splash screens...');
+  console.log("\n🤖🌊  Setting up android splash screens...");
   execSync(
-    `HYGEN_OVERWRITE=1 yarn hygen setup splashscreen android --displayName ${displayName} --bundleIdentifer ${bundleIdentifer}`,
+    `HYGEN_OVERWRITE=1 yarn hygen setup splashscreen android --displayName ${displayName} --bundleIdentifer ${bundleIdentifer}`
   );
 
-  console.log('\n🗑  Removing cruft...');
-  deleteFile('../.flowconfig');
-  deleteFile('../App.js');
-  deleteFile('../.gitattributes'); // not sure why this is here?
-  execSync('rm -rf setup', { cwd: rootDirectory });
-  execSync('rm -rf .git', { cwd: rootDirectory }); // blow away old repo if there
+  console.log("\n🗑  Removing cruft...");
+  deleteFile("../.flowconfig");
+  deleteFile("../App.js");
+  deleteFile("../.gitattributes"); // not sure why this is here?
+  execSync("rm -rf setup", { cwd: rootDirectory });
+  execSync("rm -rf .git", { cwd: rootDirectory }); // blow away old repo if there
 
   if (shouldInitializeGitRepository) {
-    console.log('\n📝 Committing project...');
+    console.log("\n📝 Committing project...");
     execSync(
       'rm -rf .git && git init && git add . && git commit -m "Initialize new React Native project."',
       {
-        cwd: rootDirectory,
-      },
+        cwd: rootDirectory
+      }
     );
 
-    console.log('\n📱 Setting initial version @0.0.1 ...');
-    execSync('npx react-native-version --never-increment-build', {
-      cwd: rootDirectory,
+    console.log("\n📱 Setting initial version @0.0.1 ...");
+    execSync("npx react-native-version --never-increment-build", {
+      cwd: rootDirectory
     });
 
-    console.log('\n📝 Committing changes...');
+    console.log("\n📝 Committing changes...");
     execSync('git add . && git commit -m "Set proper initial symver version"', {
-      cwd: rootDirectory,
+      cwd: rootDirectory
     });
   }
 
   console.log(`\n✅  Setup completed!`);
 
-  console.log('\n\n------------------------');
-  console.log('** PostInstall Notes: **');
-  console.log('------------------------\n\n');
-  console.log('\n* Check out the docs folder to customize and finalize your app!');
-  console.log('\n');
+  console.log("\n\n------------------------");
+  console.log("** PostInstall Notes: **");
+  console.log("------------------------\n\n");
+  console.log(
+    "\n* Check out the docs folder to customize and finalize your app!"
+  );
+  console.log("\n");
 }
 
 setup();
---
+++ template.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  placeholderName: 'HelloWorld',
+  titlePlaceholder: 'Hello World',
+  templateDir: './',
+  postInitScript: './setup',
+};
