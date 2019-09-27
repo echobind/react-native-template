0f977d570b6ce1c71afad7f7034c400ac6f78ebd
Joe Previte <joe@echobind.com>
2019-09-27 09:26:02 -0700

    refactor: clean up app

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,68 +1,26 @@
-// import { Provider } from 'mobx-react';
 // import ApolloClient, { InMemoryCache } from 'apollo-boost';
-import React, { Component } from 'react';
-import SplashScreen from 'react-native-splash-screen';
+import React, { Component, FC } from 'react';
+// import SplashScreen from 'react-native-splash-screen';
 import { createRootNav } from './navigation/RootNav';
-
-import Storybook from '../storybook';
 
 interface State {
   removeWhenYouveAddedStateManagement?: true;
-  /** The root store or stores to inject into the app. ONLY USED WITH MST / REDUX APPS */
-  // store: any; // if you use this, type it to your root store
   /** The configured Apollo client. ONLY USED WITH APOLLO / GRAPHQL APPS */
   // client: ApolloClient<InMemoryCache>;
 }
 
-class App extends Component<{}, State> {
-  public state = {};
+export const App: FC = () => {
 
-  public async componentDidMount() {
-    this.setupApp();
-  }
 
-  public render() {
-    // uncomment the rest of this method when setting up Apollo or MST/Redux
-    const isLoaded = this.state; // && this.state.client or this.state.store
+  // Generate the root nav of the app based on presence of a user (restored in setup functions above)
+  const currentUser = false;
+  const RootNav = createRootNav({ currentUser });
 
-    if (!isLoaded) {
-      // render nothing by default, should be covered by the splash screen
-      return null;
-    }
+  return (
 
-    // Generate the root nav of the app based on presence of a user (restored in setup functions above)
-    const currentUser = false;
-    const RootNav = createRootNav({ currentUser });
-
-    return (
-      // MST/Redux Provider:
-      // <Provider store={rootStore}>
-      //   <RootNav />
-      // </Provider>
-
-      // Apollo Provider:
-      // <ApolloProvider client={client}
-      <RootNav />
-      // </ApolloProvider>
-    );
-  }
-
-  private setupApp() {
-    try {
-      // Use this block to setup either the store or the apollo client
-      // const rootStore = await setupRootStore();
-      // this.setState({ store });
-      //
-      // const client = await setupApolloClient();
-      // this.setState({ client })
-    } catch (error) {
-      throw error;
-    } finally {
-      // Now that everything is hydrated, hide the splashscreen
-      SplashScreen.hide();
-    }
-  }
+    // Apollo Provider:
+    // <ApolloProvider client={client}
+    <RootNav />
+    // </ApolloProvider>
+  );
 }
-
-// tslint:disable-next-line
-export default App;
