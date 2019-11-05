d94110ad5227b979d59b8281c4c9d06abcd1b996
Ryan Atkinson <ryan@echobind.com>
2019-11-05 17:39:49 -0500

    add primitive Touchable component

---
+++ template/src/components/Touchable/Touchable.stories.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+
+import { Touchable } from './Touchable';
+
+storiesOf('components/Touchable', module).add('Default', () => <Touchable />);
