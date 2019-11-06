cecfb9c101b26536303fe77949f0600dd96fc2fd
Jennifer Robison <jenn@echobind.com>
2019-11-06 14:38:42 -0800

    Update LoginScreen to use Screen Component

--- template/src/screens/LoginScreen/LoginScreen.tsx
+++ template/src/screens/LoginScreen/LoginScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Alert, ImageBackground, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { Alert, ImageBackground, StatusBar, StyleSheet } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 import styled from '@emotion/native';
 
 import { Login } from '../../components/Login';
+import { Screen } from '../../components/Screen';
 import bgImage from '../../assets/images/background.png';
+import { colors } from '../../styles';
 
 const BackgroundImage = styled(ImageBackground)`
   ${StyleSheet.absoluteFillObject};
@@ -24,25 +26,19 @@ export function LoginScreen(props: NavigationScreenProps) {
 
   return (
     <BackgroundImage source={bgImage} resizeMode="cover">
-      <SafeAreaView
-        style={{
-          flex: 1,
-          justifyContent: 'flex-end',
-          margin: 20,
-        }}
-      >
         <StatusBar
           translucent
           animated
           backgroundColor="rgba(0, 0, 0, 0.20)"
           barStyle="light-content"
         />
+        <Screen backgroundColor={colors.transparent} paddingTop={60} margin={20}>
         <Login
           loginPress={loginClick}
           registrationPress={registrationClick}
           forgotPasswordPress={forgotPwdClick}
         />
-      </SafeAreaView>
+      </Screen>
     </BackgroundImage>
   );
 }
