5352fab3b6c084685306c498718e8a330930e9fa
Joe Previte <jjprevite@gmail.com>
2019-11-29 08:09:34 -0700

    fix: update eslint and prettier

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,14 +1,4 @@
 module.exports = {
   root: true,
   extends: ['plugin:echobind/react-native'],
-  extends: [
-    '@react-native-community',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
-  rules: {
-    // enable i18n first
-    // 'react/jsx-no-literals': ['error', { noStrings: true }],
-  },
 };
